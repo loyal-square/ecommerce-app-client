7dc96a6ed06a4a8b0e8a735bf46ca85da0f34396
Christopher Sy Alba <chris.alba.dev@gmail.com>
2023-04-08 21:47:40 +1200

    hopefully fix type issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import inject from '@rollup/plugin-inject';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +9,11 @@ export default defineConfig({
   resolve: {
     alias: {
       './runtimeConfig': './runtimeConfig.browser'
+    }
+  },
+  build: {
+    rollupOptions: {
+      plugins: [inject({ Buffer: ['buffer', 'Buffer'] })]
     }
   }
 });
